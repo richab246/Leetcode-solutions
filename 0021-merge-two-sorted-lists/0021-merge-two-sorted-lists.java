2c3ffa4daad0fd51ad642f617ef5eb038e63e59e
Richa Bhandari <79681567+richab246@users.noreply.github.com>
2024-06-06 21:20:52 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (79.38%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res = new ListNode(-1);
+        ListNode head1 = list1;
+        ListNode head2 = list2;
+        ListNode temp = res;
+
+        while(head1!=null && head2!=null){
+            if(head1.val <= head2.val){
+                temp.next = head1;
+                head1 = head1.next;
+            }else{
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+
+        while(head1!=null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+
+        while(head2!=null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+
+        return res.next;
+    }
+}
