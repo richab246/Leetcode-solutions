6b4aa5ca695b30fc1e1097335711b1fce80468eb
Richa Bhandari <79681567+richab246@users.noreply.github.com>
2024-06-07 15:40:50 +0530

    Time: 4 ms (100.00%), Space: 68.6 MB (6.54%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,29 +1,28 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
+        int[] res = new int[nums.length];
+        int j = 0;
 
         for(int i=0; i<nums.length; i++){
             if(nums[i]<pivot){
-                list1.add(nums[i]);
-            }else if(nums[i]>pivot){
-                list2.add(nums[i]);
+                res[j] = nums[i];
+                j++; 
             }
         }
 
         for(int i=0; i<nums.length; i++){
-            if(nums[i] == pivot){
-              list1.add(nums[i]);
+            if(nums[i]==pivot){
+                res[j] = nums[i];
+                j++; 
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>pivot){
+                res[j] = nums[i];
+                j++; 
             }
         }
 
-        list1.addAll(list2);
-        int[] result = new int[list1.size()];
-        for (int i = 0; i < list1.size(); i++) {
-            result[i] = list1.get(i);
-        }
-        
-
-       return result;
+       return res;
     }
 }
