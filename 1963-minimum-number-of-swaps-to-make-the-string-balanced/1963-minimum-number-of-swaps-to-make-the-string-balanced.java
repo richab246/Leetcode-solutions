5044165af943df68bcc8e8c0d8dfcdc40a36f58f
Richa Bhandari <79681567+richab246@users.noreply.github.com>
2024-10-09 10:28:05 +0530

    Time: 15 ms (65.17%), Space: 54 MB (84.59%) - LeetHub

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minSwaps(String s) {
+        int open = 0;
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+                if(ch == '['){
+                    open++;
+                }else{
+                    if(open>0){
+                        open--;
+                    }
+                }
+            }
+
+        return (open+1)/2;
+    }
+}
