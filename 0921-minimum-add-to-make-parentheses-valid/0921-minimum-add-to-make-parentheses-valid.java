f8027fa44369d09176062c05e789936661b34598
Richa Bhandari <79681567+richab246@users.noreply.github.com>
2024-10-09 10:12:58 +0530

    Time: 0 ms (100.00%), Space: 41 MB (95.89%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        
+       int open = 0;
+       int close = 0;
+
+       for(int i=0; i<s.length(); i++){
+         char ch = s.charAt(i);
+
+         if(ch == '('){
+            open++;
+         }else{
+            if(open>0){
+                open--;
+            }else{
+                close++;
+            }
+         }
+       } 
+
+       return open+close;
+        
+    }
+}
